089af250071f84b628d35cb1b10315c3bb08f9af
weongit <85627576+weongit@users.noreply.github.com>
2022-12-08 03:46:27 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,1 +1,35 @@
-echo Hello World1
+#!/bin/bash
+shopt -s expand_aliases
+Font_Black="\033[30m"
+Font_Red="\033[31m"
+Font_Green="\033[32m"
+Font_Yellow="\033[33m"
+Font_Blue="\033[34m"
+Font_Purple="\033[35m"
+Font_SkyBlue="\033[36m"
+Font_White="\033[37m"
+Font_Suffix="\033[0m"
+
+echo Hello
+
+
+
+countRunTimes() {
+    if [ "$is_busybox" == 1 ]; then
+        count_file=$(mktemp)
+    else
+        count_file=$(mktemp --suffix=RRC)
+    fi
+    RunTimes=$(curl -s --max-time 10 "https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fweongit%2Fhello%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" >"${count_file}")
+    TodayRunTimes=$(cat "${count_file}" | tail -3 | head -n 1 | awk '{print $5}')
+    TotalRunTimes=$(($(cat "${count_file}" | tail -3 | head -n 1 | awk '{print $7}') + 2527395))
+}
+countRunTimes
+
+
+
+
+echo -e "${Font_Green}本次测试已结束，感谢使用此脚本 ${Font_Suffix}"
+echo -e ""
+echo -e "${Font_Yellow}检测脚本当天运行次数: ${TodayRunTimes}; 共计运行次数: ${TotalRunTimes} ${Font_Suffix}"
+
